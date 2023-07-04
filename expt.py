9f95d134618eda175849acac0a34614a56b2fc6f
Ed Bartrum <edward.bartrum@gmail.com>
2023-07-04 20:19:10 +0100

    Change prompt

--- expt.py
+++ expt.py
@@ -3,7 +3,7 @@ from diffusers import StableDiffusionPipeline
 import matplotlib.pyplot as plt
 
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4").to("cuda")
-prompt = "a photograph of an astronaut riding a horse"
+prompt = "a photograph of a cat drinking coffee"
 image = pipe(prompt).images[0]
 
 image.save("out/image.png")

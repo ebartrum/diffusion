c1c2a354e9a37da9d7f52fe03467098508469af5
Ed Bartrum <edward.bartrum@gmail.com>
2023-07-04 18:50:50 +0100

    Show output with matplotlib

--- expt.py
+++ expt.py
@@ -1,5 +1,8 @@
 from diffusers import DDPMPipeline
+import matplotlib.pyplot as plt
 
 ddpm = DDPMPipeline.from_pretrained("google/ddpm-cat-256").to("cuda")
 image = ddpm(num_inference_steps=25).images[0]
 image.save("out/image.png")
+plt.imshow(image)
+plt.show()

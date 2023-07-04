df423f2e2e01587602ee996950d4b197072113ed
Ed Bartrum <edward.bartrum@gmail.com>
2023-07-04 20:16:44 +0100

    Change example to use stable diffusion

--- expt.py
+++ expt.py
@@ -1,8 +1,11 @@
-from diffusers import DDPMPipeline
+import torch
+from diffusers import StableDiffusionPipeline
 import matplotlib.pyplot as plt
 
-ddpm = DDPMPipeline.from_pretrained("google/ddpm-cat-256").to("cuda")
-image = ddpm(num_inference_steps=25).images[0]
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4").to("cuda")
+prompt = "a photograph of an astronaut riding a horse"
+image = pipe(prompt).images[0]
+
 image.save("out/image.png")
 plt.imshow(image)
 plt.show()

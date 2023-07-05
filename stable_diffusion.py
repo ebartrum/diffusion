41262e47bb21f891cb5d33b8be6ed06891ae5aa5
Ed Bartrum <edward.bartrum@gmail.com>
2023-07-05 17:24:24 +0100

    Update SD options

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -10,9 +10,9 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id).to(device)
 generator = torch.Generator(device=device).manual_seed(42)
 # Run the pipeline, showing some of the available arguments
 pipe_output = pipe(
-    prompt="Palette knife painting of an autumn cityscape", # What to generate
+    prompt="Palette knife painting of 2 cats playing chess", # What to generate
     negative_prompt="Oversaturated, blurry, low quality", # What NOT to generate
-    height=480, width=640,     # Specify the image size
+    height=480, width=480,     # Specify the image size
     guidance_scale=8,          # How strongly to follow the prompt
     num_inference_steps=35,    # How many steps to take
     generator=generator        # Fixed random seed

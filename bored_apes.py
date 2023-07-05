ad59bbaea6ebc7ce257114aefc3a84a2f65db147
Ed Bartrum <edward.bartrum@gmail.com>
2023-07-05 13:12:12 +0100

    use cuda

--- bored_apes.py
+++ bored_apes.py
@@ -1,8 +1,8 @@
 from diffusers import DDPMPipeline
+import matplotlib.pyplot as plt
 
-pipeline = DDPMPipeline.from_pretrained('CCMat/diff-bored-apes-128')
+pipeline = DDPMPipeline.from_pretrained('CCMat/diff-bored-apes-128').to("cuda")
 image = pipeline().images[0]
 
-import matplotlib.pyplot as plt
 plt.imshow(image)
 plt.show()

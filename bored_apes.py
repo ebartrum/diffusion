10a3f2a792dbd0f1e6819df8bf61a6b28c2e282c
Ed Bartrum <edward.bartrum@gmail.com>
2023-07-05 13:19:57 +0100

    Update num steps

--- bored_apes.py
+++ bored_apes.py
@@ -1,8 +1,8 @@
 from diffusers import DDPMPipeline
 import matplotlib.pyplot as plt
 
-pipeline = DDPMPipeline.from_pretrained('CCMat/diff-bored-apes-128').to("cuda")
-image = pipeline().images[0]
+ddpm = DDPMPipeline.from_pretrained('CCMat/diff-bored-apes-128').to("cuda")
+image = ddpm(num_inference_steps=700).images[0]
 
 plt.imshow(image)
 plt.show()

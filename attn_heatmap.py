56ab25c58ac0e8ad6012ada9734d0c291c4e510e
Ed Bartrum <edward.bartrum@gmail.com>
2023-07-06 18:41:06 +0100

    Use a different SD model

--- attn_heatmap.py
+++ attn_heatmap.py
@@ -3,15 +3,14 @@ from diffusers import StableDiffusionPipeline
 from matplotlib import pyplot as plt
 import torch
 
-model_id = 'stabilityai/stable-diffusion-2-base'
 device = 'cuda'
-pipe = StableDiffusionPipeline.from_pretrained(model_id)
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5").to(device)
 pipe = pipe.to(device)
 
 prompt = 'A dog runs across the field'
 gen = set_seed(0)  # for reproducibility
 
-with torch.cuda.amp.autocast(dtype=torch.float16), torch.no_grad():
+with torch.no_grad():
     with trace(pipe) as tc:
         out = pipe(prompt, num_inference_steps=30, generator=gen)
         heat_map = tc.compute_global_heat_map()

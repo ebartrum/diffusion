609f5f736a54563d249101a1fc7af87b69ccb416
Ed Bartrum <edward.bartrum@gmail.com>
2023-07-23 17:55:07 +0100

    Add dilation

--- sd_inpainting.py
+++ sd_inpainting.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 from torchvision.utils import save_image
 import numpy as np
 from diffusers import StableDiffusionInpaintPipeline
@@ -17,12 +18,23 @@ def load_image(filename, size=None):
         img = img.resize(size)
     return img
 
+def refine_mask(mask, dilation=2):
+    res = mask.shape[-1]
+    # binarise
+    mask = (mask>0.5).float()
+    # dilate
+    mask = F.max_pool2d(mask.unsqueeze(0),dilation).squeeze(0)
+    mask = F.interpolate(mask.unsqueeze(0).unsqueeze(0),
+         res).squeeze(0).squeeze(0)
+    return mask
+
 init_image = load_image("data/statue_scene/IMG_2707.jpg").resize((512, 512))
 mask_image = load_image("data/statue_segmentation/000.png").resize((512, 512))
 init_image = torch.from_numpy(np.array(init_image)).permute(2,0,1)/255
 mask_image = torch.from_numpy(np.array(mask_image)).permute(2,0,1)[0]/255
+
+mask_image = refine_mask(mask_image, dilation=32)
 init_image = 2*init_image - 1
-mask_image = (mask_image>0.5).float()
 
 # Load the pipeline
 device = "cuda"

3dcfec609e855d75d4df27caba7c9ee1491c24e4
Ed Bartrum <edward.bartrum@gmail.com>
2023-12-04 15:26:02 +0000

    Update the prompt. Change the output dir. Remove comments

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -1,27 +1,26 @@
 import torch
 from diffusers import StableDiffusionPipeline
 import matplotlib.pyplot as plt
+import os
 
-# Load the pipeline
 device = "cuda"
 model_id = "stabilityai/stable-diffusion-2-1-base"
 pipe = StableDiffusionPipeline.from_pretrained(model_id).to(device)
 
 generator = torch.Generator(device=device).manual_seed(42)
-# Run the pipeline, showing some of the available arguments
 pipe_output = pipe(
-    prompt="Palette knife painting of 2 cats playing chess", # What to generate
-    negative_prompt="Oversaturated, blurry, low quality", # What NOT to generate
-    height=480, width=480,     # Specify the image size
-    guidance_scale=8,          # How strongly to follow the prompt
-    num_inference_steps=35,    # How many steps to take
-    generator=generator        # Fixed random seed
+    prompt="A cute and realistic kitten",
+    negative_prompt="Oversaturated, blurry, low quality",
+    height=512, width=512,
+    guidance_scale=8,
+    num_inference_steps=35,
+    generator=generator
 )
 
-# View the resulting image:
 image = pipe_output.images[0]
 
 # Log output
-image.save("out/sd.png")
+output_dir = "runs"
+image.save(os.path.join("out","sd.png"))
 plt.imshow(image)
 plt.show()

d728ccbf5cdac4cc8ae116ae4872e2e3c2dd5d90
Ed Bartrum <edward.bartrum@gmail.com>
2023-12-06 14:34:55 +0000

    Remove many old files

--- bored_apes.py
+++
@@ -1,8 +0,0 @@
-from diffusers import DDPMPipeline
-import matplotlib.pyplot as plt
-
-ddpm = DDPMPipeline.from_pretrained('CCMat/diff-bored-apes-128').to("cuda")
-image = ddpm(num_inference_steps=700).images[0]
-
-plt.imshow(image)
-plt.show()

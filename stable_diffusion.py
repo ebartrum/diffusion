225ba3139ae1eae90934d1642441565f9c86c726
Ed Bartrum <edward.bartrum@gmail.com>
2023-12-06 15:48:37 +0000

    Add Hydra cfg

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -1,35 +1,42 @@
-import os
-from utils import SLURM_OUTPUT_DIR
-import torch
 from diffusers import StableDiffusionPipeline, DDIMScheduler
+from omegaconf import DictConfig, OmegaConf
+from utils import SLURM_OUTPUT_DIR
+import hydra
+import yaml
+import os
 import matplotlib.pyplot as plt
+import torch
 
-if os.getenv("SLURM_JOB_ID"):
-    output_dir = os.path.join("out", SLURM_OUTPUT_DIR)
+@hydra.main(config_path="conf",
+            config_name="config", version_base=None)
+def main(cfg):
+    if os.getenv("SLURM_JOB_ID"):
+        output_dir = os.path.join("out", SLURM_OUTPUT_DIR)
 
-else:
-    output_dir = "out"
-os.makedirs(output_dir, exist_ok=True)
+    else:
+        output_dir = "out"
+    os.makedirs(output_dir, exist_ok=True)
+    with open(os.path.join(output_dir,"cfg.yaml"), 'w') as f:
+        f.write(OmegaConf.to_yaml(cfg))
 
-device = "cuda"
-model_id = "stabilityai/stable-diffusion-2-1-base"
-model_dir = "./models"
-ddim = DDIMScheduler.from_pretrained(
-       model_id, subfolder="scheduler",
-       cache_dir=model_dir, local_files_only=True)
-pipe = StableDiffusionPipeline.from_pretrained(model_id,schedule=ddim,
-       cache_dir=model_dir, local_files_only=True).to(device)
+    device = "cuda"
+    ddim = DDIMScheduler.from_pretrained(
+           cfg.model_id, subfolder="scheduler",
+           cache_dir=cfg.model_dir, local_files_only=True)
+    pipe = StableDiffusionPipeline.from_pretrained(cfg.model_id,schedule=ddim,
+           cache_dir=cfg.model_dir, local_files_only=True).to(device)
 
-generator = torch.Generator(device=device).manual_seed(42)
-pipe_output = pipe(
-    prompt="A cute and realistic kitten",
-    negative_prompt="Oversaturated, blurry, low quality",
-    height=512, width=512,
-    guidance_scale=8,
-    num_inference_steps=35,
-    generator=generator
-)
-image = pipe_output.images[0]
+    generator = torch.Generator(device=device).manual_seed(42)
+    pipe_output = pipe(
+        prompt="A cute and realistic kitten",
+        negative_prompt="Oversaturated, blurry, low quality",
+        height=512, width=512,
+        guidance_scale=8,
+        num_inference_steps=35,
+        generator=generator
+    )
+    image = pipe_output.images[0]
+    image.save(os.path.join(output_dir,"sd.png"))
 
-# Log output
-image.save(os.path.join(output_dir,"sd.png"))
+if __name__ == "__main__":
+    main()

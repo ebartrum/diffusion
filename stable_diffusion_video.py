f856739e08be7e5fa7fe02fe4acb4707c9128454
Ed Bartrum <edward.bartrum@gmail.com>
2023-12-07 15:12:34 +0000

    Load the model weights for models dir

--- stable_diffusion_video.py
+++ stable_diffusion_video.py
@@ -3,8 +3,10 @@ import torch
 from diffusers import StableVideoDiffusionPipeline
 from diffusers.utils import load_image, export_to_video
 
+model_dir, local_files_only = "./models", True
 pipe = StableVideoDiffusionPipeline.from_pretrained(
-    "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16"
+    "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16",
+    cache_dir=model_dir, local_files_only=local_files_only
 )
 pipe.enable_model_cpu_offload()
 

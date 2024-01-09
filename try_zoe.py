274783e9549d0a4758dab5fdfe1d93dd03efb277
Ed Bartrum <edward.bartrum@gmail.com>
2024-01-09 13:56:16 +0000

    Update the output dir

--- try_zoe.py
+++ try_zoe.py
@@ -1,9 +1,16 @@
+import os
 import torch
 from PIL import Image
 from submodules.zoedepth.zoedepth.utils.misc import (colorize,
     pil_to_batched_tensor, save_raw_16bit, get_image_from_url)
 from submodules.zoedepth.zoedepth.utils.config import get_config
 from submodules.zoedepth.zoedepth.models.builder import build_model
+
+if os.getenv("SLURM_JOB_ID"):
+    output_dir = os.path.join("out", SLURM_OUTPUT_DIR)
+else:
+    output_dir = "out"
+os.makedirs(output_dir, exist_ok=True)
 
 conf = get_config("zoedepth", "infer")
 model_zoe_n = build_model(conf)
@@ -24,10 +31,10 @@ depth_tensor = zoe.infer(X)
 depth = zoe.infer_pil(image)
 
 # Save raw
-fpath = "out/zoe_output.png"
+fpath = os.path.join(output_dir, "zoe_output.png")
 grey_depth = colorize(depth, cmap="gray_r")
 Image.fromarray(grey_depth).save(fpath)
 
 colored = colorize(depth, cmap="magma_r")
-fpath_colored = "out/zoe_output_colored.png"
+fpath_colored = os.path.join(output_dir, "zoe_output_colored.png")
 Image.fromarray(colored).save(fpath_colored)

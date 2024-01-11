86626173a9cb61a18e4c15314e92d91ebbe76a72
Ed Bartrum <edward.bartrum@gmail.com>
2024-01-11 10:42:16 +0000

    Add a couple of new data images

--- sd_inpainting.py
+++ sd_inpainting.py
@@ -1,13 +1,22 @@
 import torch
 from PIL import Image
-from diffusers import StableDiffusionInpaintPipeline
-image = Image.open("misc/img_2730.png").convert("RGB")
-mask_image = Image.open("misc/mask_2730.png").convert("RGB")
+from diffusers import StableDiffusionInpaintPipeline, AutoPipelineForInpainting
 
-pipe = StableDiffusionInpaintPipeline.from_pretrained(
-    "runwayml/stable-diffusion-inpainting",
-    torch_dtype=torch.float32,
-)
-prompt = "empty plinth"
-image = pipe(prompt=prompt, image=image, mask_image=mask_image).images[0]
+image = Image.open("misc/rednet_img.png").convert("RGB")
+mask_image = Image.open("misc/rednet_mask.png").convert("RGB")
+
+# pipe = StableDiffusionInpaintPipeline.from_pretrained(
+#     # "runwayml/stable-diffusion-inpainting",
+#     "stabilityai/stable-diffusion-2-inpainting",
+#     torch_dtype=torch.float32,
+# )
+pipe = AutoPipelineForInpainting.from_pretrained(
+      "diffusers/stable-diffusion-xl-1.0-inpainting-0.1",
+      torch_dtype=torch.float16,
+      variant="fp16").to("cuda")
+
+# prompt = "empty plinth"
+prompt = ""
+image = pipe(prompt=prompt, image=image, mask_image=mask_image,
+             guidance_scale=7.5).images[0]
 image.save("out/inpainted.png")

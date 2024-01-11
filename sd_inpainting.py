94aa31f1f28aa6ecba1a8dede97161b258734076
Ed Bartrum <edward.bartrum@gmail.com>
2024-01-11 10:54:51 +0000

    Start adding cfg to the inpainting script

--- sd_inpainting.py
+++ sd_inpainting.py
@@ -1,22 +1,29 @@
 import torch
 from PIL import Image
 from diffusers import StableDiffusionInpaintPipeline, AutoPipelineForInpainting
+import hydra
 
-image = Image.open("misc/rednet_img.png").convert("RGB")
-mask_image = Image.open("misc/rednet_mask.png").convert("RGB")
+@hydra.main(config_path="conf/inpainting",
+            config_name="config", version_base=None)
+def main(cfg):
+    image = Image.open("misc/rednet_img.png").convert("RGB")
+    mask_image = Image.open("misc/rednet_mask.png").convert("RGB")
 
-# pipe = StableDiffusionInpaintPipeline.from_pretrained(
-#     # "runwayml/stable-diffusion-inpainting",
-#     "stabilityai/stable-diffusion-2-inpainting",
-#     torch_dtype=torch.float32,
-# )
-pipe = AutoPipelineForInpainting.from_pretrained(
-      "diffusers/stable-diffusion-xl-1.0-inpainting-0.1",
-      torch_dtype=torch.float16,
-      variant="fp16").to("cuda")
+    # pipe = StableDiffusionInpaintPipeline.from_pretrained(
+    #     # "runwayml/stable-diffusion-inpainting",
+    #     "stabilityai/stable-diffusion-2-inpainting",
+    #     torch_dtype=torch.float32,
+    # )
+    pipe = AutoPipelineForInpainting.from_pretrained(
+          "diffusers/stable-diffusion-xl-1.0-inpainting-0.1",
+          torch_dtype=torch.float16,
+          variant="fp16").to("cuda")
 
-# prompt = "empty plinth"
-prompt = ""
-image = pipe(prompt=prompt, image=image, mask_image=mask_image,
-             guidance_scale=7.5).images[0]
-image.save("out/inpainted.png")
+    # prompt = "empty plinth"
+    prompt = ""
+    image = pipe(prompt=prompt, image=image, mask_image=mask_image,
+                 guidance_scale=7.5).images[0]
+    image.save("out/inpainted.png")
+
+if __name__ == "__main__":
+    main()

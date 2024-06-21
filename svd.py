e7c9ec906b3fd1d06c7d6273a37e569910bbfa76
Edward Bartrum <edward.bartrum@gmail.com>
2024-06-21 18:26:31 +0900

    Switch the stable video diffusion file

--- svd.py
+++ svd.py
@@ -1,16 +1,16 @@
 import torch
+
 from diffusers import StableVideoDiffusionPipeline
 from diffusers.utils import load_image, export_to_video
+from PIL import Image
 
-model_dir, local_files_only = "./models", True
 pipe = StableVideoDiffusionPipeline.from_pretrained(
-    "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16",
-    cache_dir=model_dir, local_files_only=local_files_only
+    "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16"
 )
 pipe.enable_model_cpu_offload()
 
 # Load the conditioning image
-image = load_image("./misc/rocket.png")
+image = Image.open("../gaussian-splatting/data/face/images/frame_00044.jpg")
 image = image.resize((1024, 576))
 
 generator = torch.manual_seed(42)

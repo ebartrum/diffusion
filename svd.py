ea106518df2c1b54de71a8da00b47615049bb88d
Edward Bartrum <edward.bartrum@gmail.com>
2024-06-21 18:54:25 +0900

    Use OmegaConf

--- svd.py
+++ svd.py
@@ -1,19 +1,31 @@
 import torch
-
+from omegaconf import OmegaConf
 from diffusers import StableVideoDiffusionPipeline
 from diffusers.utils import load_image, export_to_video
 from PIL import Image
+from dataclasses import dataclass
 
 pipe = StableVideoDiffusionPipeline.from_pretrained(
     "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16"
 )
 pipe.enable_model_cpu_offload()
 
+@dataclass
+class Config:
+    motion_bucket_id: int = 60
+    output_file: str = "generated.mp4"
+    image_path: str = "../gaussian-splatting/data/face/images/frame_00044.jpg"
+
+default_cfg = OmegaConf.structured(Config)
+cli_cfg = OmegaConf.from_cli()
+cfg = OmegaConf.merge(default_cfg, cli_cfg)
+
 # Load the conditioning image
 image = Image.open("../gaussian-splatting/data/face/images/frame_00044.jpg")
 image = image.resize((1024, 576))
 
 generator = torch.manual_seed(42)
-frames = pipe(image, decode_chunk_size=8, generator=generator, motion_bucket_id=60).frames[0]
+frames = pipe(image, decode_chunk_size=8, generator=generator,
+  motion_bucket_id=cfg.motion_bucket_id).frames[0]
 
-export_to_video(frames, "out/generated.mp4", fps=7)
+export_to_video(frames, f"out/{cfg.output_file}", fps=7)

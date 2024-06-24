09ec5e8093a90eb6f1368b071f61c55f6d74a793
Edward Bartrum <edward.bartrum@gmail.com>
2024-06-24 15:07:54 +0900

    Save the 2 videos

--- svd.py
+++ svd.py
@@ -14,19 +14,24 @@ pipe.enable_model_cpu_offload()
 @dataclass
 class DefaultConfig:
     motion_bucket_id: int = 60
-    output_file: str = "generated.mp4"
+    output_file0: str = "generated0.mp4"
+    output_file1: str = "generated1.mp4"
     image_path: str = "../gaussian-splatting/data/face/images/frame_00044.jpg"
 
 cfg = OmegaConf.merge(OmegaConf.structured(DefaultConfig),
       OmegaConf.from_cli())
 
-# Load the conditioning image
-image = Image.open("../gaussian-splatting/data/face/images/frame_00044.jpg")
-image = image.resize((1024, 576))
+# Load the conditioning images
+img1 = Image.open("../gaussian-splatting/data/face/images/frame_00044.jpg")
+img1 = img1.resize((1024, 576))
+img2 = Image.open("../gaussian-splatting/data/face/images/frame_00045.jpg")
+img2 = img2.resize((1024, 576))
 
 generator = torch.manual_seed(42)
-frames = new_call(pipe, image, decode_chunk_size=8, generator=generator,
-  motion_bucket_id=cfg.motion_bucket_id).frames[0]
+frames = new_call(pipe, [img1,img2], decode_chunk_size=8, generator=generator,
+  motion_bucket_id=cfg.motion_bucket_id).frames
 
-print(f"Saving video to out/{cfg.output_file} ...")
-export_to_video(frames, f"out/{cfg.output_file}", fps=7)
+print(f"Saving video 0 to out/{cfg.output_file0} ...")
+export_to_video(frames[0], f"out/{cfg.output_file0}", fps=7)
+print(f"Saving video 1 to out/{cfg.output_file1} ...")
+export_to_video(frames[1], f"out/{cfg.output_file1}", fps=7)

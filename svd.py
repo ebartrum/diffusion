4ee56d89ff97116d84e09488be4b0bcff4f88af1
Edward Bartrum <edward.bartrum@gmail.com>
2024-06-24 14:49:01 +0900

    Extract a function for calling the video pipeline

--- svd.py
+++ svd.py
@@ -4,6 +4,7 @@ from diffusers import StableVideoDiffusionPipeline
 from diffusers.utils import load_image, export_to_video
 from PIL import Image
 from dataclasses import dataclass
+from svd_logic_hacking import new_call
 
 pipe = StableVideoDiffusionPipeline.from_pretrained(
     "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16"
@@ -24,7 +25,8 @@ image = Image.open("../gaussian-splatting/data/face/images/frame_00044.jpg")
 image = image.resize((1024, 576))
 
 generator = torch.manual_seed(42)
-frames = pipe(image, decode_chunk_size=8, generator=generator,
+frames = new_call(pipe, image, decode_chunk_size=8, generator=generator,
   motion_bucket_id=cfg.motion_bucket_id).frames[0]
 
+print(f"Saving video to out/{cfg.output_file} ...")
 export_to_video(frames, f"out/{cfg.output_file}", fps=7)
